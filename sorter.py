ffc14022e2e66a303d3f6d95b6cac37437e9281c
rrviens <39856416+rrviens@users.noreply.github.com>
2019-09-14 22:01:45 +0300

    Add files via upload

---
+++ sorter.py
@@ -0,0 +1,8 @@
+filepath = 'D:/Desktop/Everything/Programming/GitHub/Python Projects/py sorter/sortnumber.txt'
+with open(filepath) as fp:
+    line = fp.readline()
+    splitter = line.split(" ")
+    splitter = list(map(int, splitter))
+
+splitter.sort()
+print(splitter)
